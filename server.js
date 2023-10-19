341fddb2453b893c12dd847034db9bd9583337f5
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-10-19 16:12:23 -0400

    declared global vars and middleware

---
+++ server.js
@@ -0,0 +1,27 @@
+const { log } = require('console');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+const { v4: uuidv4 } = require('uuid');
+const app = express();
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Develop/db/db.json'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/Develop/public/index.html'));
+});
+
+
+
+
+
+
+
