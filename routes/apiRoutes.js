ab327d4f38e8b13b4b4534a51efcd3089b9c903f
MadeleineA42 <abraham.madeleine229@gmail.com>
2023-10-24 16:54:04 -0400

    added script to pkgjson, fixed delete function and updated pathing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,25 +1,51 @@
-const fs = require('fs');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const router = express.Router();
+// const fs = require('fs');
+// const path = require('path');
+// const { v4: uuidv4 } = require('uuid');
 
-module.exports = (app) =>   {
+// const app = express();
+// router.get('/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/notes.html'));
+// })
 
-app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'));
-});
+// router.get('/api/notes', (req, res) => {
+//     const db = JSON.parse(fs.readFileSync('db/db.json', 'utf8')); 
+//     res.json(db);
+// });
 
-app.post('/api/notes', (req, res) => {
-    const db = JSON.parse(fs.readFileSync('db/db.json'));
-    const inputNote = {
-        title: req.body.title,
-        text: req.body.text,
-        id: uuidv4(),
-    };
-    db.push(inputNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(db));
-    res.json(db);
-});
-}
+// router.post('/api/notes', (req, res) => {
+//     const db = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+//     const inputNote = {
+//         title: req.body.title,
+//         text: req.body.text,
+//         id: uuidv4(),
+//     };
+//     db.push(inputNote);
+//     fs.writeFileSync('db/db.json', JSON.stringify(db));
+//     res.json(db);
+// });
+
+// module.exports = router;
+
+// app.get('/notes', (req, res) => {
+//     res.sendFile(__dirname, './public/notes.html');
+// });
+
+// app.get('/api/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, './db/db.json'));
+// });
+
+// app.post('/api/notes', (req, res) => {
+//     const db = JSON.parse(fs.readFileSync('./db/db.json'));
+//     const inputNote = {
+//         title: req.body.title,
+//         text: req.body.text,
+//         id: uuidv4(),
+//     };
+//     db.push(inputNote);
+//     fs.writeFileSync('./db/db.json', JSON.stringify(db));
+//     res.json(db);
+// });
+
 // module.exports = app;
--- server.js
+++ server.js
@@ -1,15 +1,54 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+
 
 //middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/api', htmlRoutes);
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
+
+app.get('/api/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './db/db.json'));
+});
+
+app.post('/api/notes', (req, res) => {
+    const db = JSON.parse(fs.readFileSync('./db/db.json'));
+    const inputNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    };
+    db.push(inputNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(db));
+    res.json(db);
+});
+
+app.delete('/api/notes/:id', (req, res) =>  {
+    let db = JSON.parse(fs.readFileSync('./db/db.json'));
+    const noteID = req.params.id;
+    const noteIndex = db.findIndex(note => note.id === noteID);
+
+    if (noteIndex !== -1)   {
+        db.splice(noteIndex, 1);
+        fs.writeFileSync('./db/db.json', JSON.stringify(db));
+        res.json({ message: 'This note is gone forever!' });
+    } else {
+        res.status(404).json({ error: 'Note not found' });
+    }
+});
 
 app.listen(PORT, () => {
     console.log(`Now listening on PORT ${PORT}`);
